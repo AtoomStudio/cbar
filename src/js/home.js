649599442e1f83c737348a5a9444c1e876817c63
Ignasi <ignasireixach@gmail.com>
2022-12-28 11:13:42 +0100

    Add provider slider in home

--- src/js/home.js
+++ src/js/home.js
@@ -5,10 +5,13 @@ import collapseGrid from "./modules/collapseGridHalf.js";
 import carouselGrid from "./modules/carouselGrid.js";
 import favHeart from "./modules/favHeart.js";
 import searchGames from "./modules/search-games.js";
+import carouselLandingSEO from "./modules/carouselLandingSEO.js";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   carouselBanner();
   carouselJackpot();
+  carouselLandingSEO();
   // carouselBets();
   favHeart().init();
   collapseGrid();
