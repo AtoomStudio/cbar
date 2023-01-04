10f2bd2306da016a022b871540f1d5f3bf368a30
Josep Camps <jcamps@atoomstudio.com>
2023-01-04 16:39:19 +0100

    Add bonusCode manager

---
+++ src/js/modules/bonusCodeCTA.js
@@ -0,0 +1,48 @@
+const bonusCTAs = document.querySelectorAll('[data-bonus-cta]');
+
+function bindCTAs() {
+    bonusCTAs.forEach((cta) => {
+        cta.addEventListener('click', async () => {
+            const code = cta.dataset.bonusCtaCode;
+            const redirect = cta.dataset.bonusCtaRedirect;
+            const successText = cta.dataset.bonusCtaSuccess;
+            const errorText = cta.dataset.bonusCtaError;
+            cta.classList.add('btn--loading');
+            try {
+                await sendCoupon(code);
+                cta.classList.add('btn--disabled');
+                if (successText) {
+                    cta.innerHTML = successText;
+                }
+                if (redirect) {
+                    window.location.href = redirect;
+                }
+            } catch (error) {
+                if (errorText) {
+                    cta.classList.add('btn--disabled');
+                    cta.innerHTML = errorText;
+                }
+                console.log("No se ha podido aplicar el código", "error");
+            } finally {
+                cta.classList.remove('btn--loading');
+            }
+        });
+    });
+}
+
+async function sendCoupon(code) {
+    try {
+        const response = await fetch('/members/bonusCode.jsp', { method: 'POST', body: `bonusCode=${code}` });
+        return response.json();
+    } catch (error) {
+        console.log("Error fetching bonusCode", error);
+    }
+}
+
+function bonusCodeCTA() {
+    if (bonusCTAs) {
+        bindCTAs();
+    }
+}
+
+export default bonusCodeCTA;
--- src/js/promocion.js
+++ src/js/promocion.js
@@ -1,8 +1,10 @@
 import accordion from "./modules/accordion.js";
+import bonusCodeCTA from "./modules/bonusCodeCTA.js";
 import countdown from "./modules/countdown.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   accordion();
   countdown();
+  bonusCodeCTA();
 });
 
