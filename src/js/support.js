07a89969498763947c9c5b42f6d16488b39e9efc
Ignasi <ignasireixach@gmail.com>
2023-01-17 17:49:14 +0100

    Toggle btn--loading class on submit button

--- src/js/support.js
+++ src/js/support.js
@@ -3,17 +3,29 @@ import historyNavBar from "./modules/historyNavBar.js";
 
 function initSupportForm() {
   const theForm = document.querySelector("#supportForm")
+  const button = document.querySelector('.btn__fontSm')
+
   const alertMgr = Alert();
+
+  button.addEventListener('click', () => {
+    button.classList.toggle('btn--loading')
+  })
 
   const sendMail = async () => {
     const response = await fetch("https://api.casinobarcelona.es/mail/support/send", {
       method: "post",
       body: new FormData(theForm),
     })
+
+    
     if (response.ok) {
       alertMgr.add("Consulta enviada", "success")
+        button.classList.toggle('btn--loading')
+
     } else {
       alertMgr.add("Algo ha salido mal, prueba mas tarde o abre el chat", "error")
+        button.classList.toggle('btn--loading')
+
     }
   }
 
