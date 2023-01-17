ee4e6b2436280b79eba774b4f59df809910f7d2b
Ignasi <ignasireixach@gmail.com>
2023-01-17 12:39:07 +0100

    Delete gsap in popUpBalance, and collapseGridHalf

--- src/js/modules/popUpBalance.js
+++ src/js/modules/popUpBalance.js
@@ -1,9 +1,8 @@
-import { gsap } from "gsap";
-
 function popUpSaldo() {
   const popUpCloseBtn = document.querySelector(".popUpBalance__closeBtn");
   const popUpOpenBtns = document.querySelectorAll(".popUpBalance__openBtn");
   const popUpMenu = document.querySelector(".popUpBalance");
+
   const header = document.querySelector(".header");
 
   if (popUpOpenBtns.length === 0) return;
@@ -11,44 +10,15 @@ function popUpSaldo() {
   popUpMenu.style.marginTop = `${header.offsetHeight}px`;
 
   popUpOpenBtns.forEach((btn) => {
-    if (window.innerWidth < 1280) {
-      btn.addEventListener("click", () => {
-        gsap
-          .timeline()
-          .to(popUpMenu, { display: "block", duration: 0 })
-          .to(popUpMenu, {
-            opacity: 1,
-            transform: "translateY(0)",
-            duration: 0.4,
-          });
-      });
-    } else {
-      btn.addEventListener("click", () => {
-        gsap
-          .timeline()
-          .to(popUpMenu, { display: "block", duration: 0 })
-          .to(popUpMenu, { opacity: 1, duration: 0.4 });
-      });
-    }
-  });
+    btn.addEventListener("click", () => {
+        popUpMenu.classList.toggle('popUpBalance--openMenu')
+    })
+   
+  })
 
   popUpCloseBtn.addEventListener("click", () => {
-    if (window.innerWidth < 1280) {
-      gsap
-        .timeline()
-        .to(popUpMenu, {
-          opacity: 0,
-          transform: "translateY(100%)",
-          duration: 0.3,
-        })
-        .to(popUpMenu, { display: "none", duration: 0 });
-    } else {
-      gsap
-        .timeline()
-        .to(popUpMenu, { opacity: 0, duration: 0.2 })
-        .to(popUpMenu, { display: "none", duration: 0 });
-    }
-  });
+    popUpMenu.classList.remove('popUpBalance--openMenu')
+  })
 }
 
 export default popUpSaldo;
