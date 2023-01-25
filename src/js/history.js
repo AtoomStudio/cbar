d31f7ee02e74e118de05f95709c6cdeb1d8d5686
Ignasi <ignasireixach@gmail.com>
2023-01-25 13:28:10 +0100

    Remove carouselMenuHistory

--- src/js/history.js
+++ src/js/history.js
@@ -1,9 +1,7 @@
-import carouselMenuHistory from "./modules/carouselMenuHistory.js";
 import calendarHistory from "./modules/calendarHistory.js";
 import historyNavBar from "./modules/historyNavBar.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   historyNavBar();
-  carouselMenuHistory();
   calendarHistory();
 });
