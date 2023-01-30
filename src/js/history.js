f703c968dbaef740850532cb6e68cf8c8bdcdf1b
Ignasi <ignasireixach@gmail.com>
2023-01-30 12:10:05 +0100

    Define activeToggle cls, fix overflow pagination

--- src/js/history.js
+++ src/js/history.js
@@ -1,7 +1,9 @@
+import activeToggle from "./modules/activeToggle.js";
 import calendarHistory from "./modules/calendarHistory.js";
 import historyNavBar from "./modules/historyNavBar.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   historyNavBar();
   calendarHistory();
+  activeToggle('.paginator__btn')
 });
