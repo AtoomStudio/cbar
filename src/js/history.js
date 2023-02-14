fef392467afb47de5f4ca015db5aceeebfebf6e0
Josep Camps <jcamps@atoomstudio.com>
2023-02-14 14:51:10 +0100

    Show active item in historial menu

--- src/js/history.js
+++ src/js/history.js
@@ -6,4 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
   historyNavBar();
   calendarHistory();
   pagination();
+
+  const activeNav = document.querySelector('.history__nav.is-active')
+  if(!activeNav) return;
+  activeNav.scrollIntoView({
+    block: "center",
+    inline: "center"
+  })
 });
