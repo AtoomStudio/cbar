fa1eca7f4fdeb849ec94955ab07fc805f87597ea
Ignasi <ignasireixach@gmail.com>
2023-02-15 11:48:36 +0100

    Fix class promociones

--- src/js/promociones.js
+++ src/js/promociones.js
@@ -3,7 +3,7 @@ import carouselPromociones from "./modules/carouselPromociones.js";
 document.addEventListener('DOMContentLoaded', () => {
   carouselPromociones();
 
-  const activeNav = document.querySelector('promo__navbar--item.is-active')
+  const activeNav = document.querySelector('.promo__navbar--item.is-active')
   if(!activeNav) return;
   activeNav.scrollIntoView({
     block: "center",
