1b8a434cb26fdb94fb6aeb87869c2b19383ed712
Josep Camps <jcamps@atoomstudio.com>
2023-02-16 00:57:46 +0100

    Add game review feature

---
+++ src/js/modules/gameReview.js
@@ -0,0 +1,51 @@
+function gameReview() {
+  const reviewEl = document.querySelector('.reviewsScreen');
+  const starsEls = document.querySelectorAll('.reviewsScreen__star');
+  const countEl = document.querySelector('.reviewsScreen__score .reviewsScreen__count span');
+  const userScoreEl = document.querySelector('.reviewsScreen__user-rating .reviewsScreen__average');
+  let isRated = reviewEl.classList.contains('reviewsScreen--rated');
+
+  function bindEvents() {
+
+    if (isRated) return;
+
+    starsEls.forEach(starEl => {
+      starEl.addEventListener('click', handleStarClick);
+    });
+  }
+
+  function handleStarClick(e) {
+    const star = e.currentTarget;
+    const score = star.dataset.score;
+    sendScore(score);
+    userScoreEl.textContent = score;
+    starsEls.forEach(starEl => {
+      starEl.classList.remove('reviewsScreen__star--active');
+    });
+    star.classList.add('reviewsScreen__star--active');
+
+    if (isRated) return;
+    countEl.textContent = parseInt(countEl.textContent) + 1;
+    reviewEl.classList.add('reviewsScreen--rated');
+    isRated = true;
+
+  }
+
+  function sendScore(score) {
+    const room = reviewEl.dataset.roomId;
+    const url = "/servlet/RankingServlet";
+    fetch(`${url}?room=${room}&ranking=${score}`)
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+
+  function init() {
+    bindEvents();
+  }
+
+  return {
+    init
+  };
+}
+
+export default gameReview;
--- src/js/screens.js
+++ src/js/screens.js
@@ -1,11 +1,13 @@
 import DemoBar from "./modules/demo-bar.js";
 import disableScreen from "./modules/disableScreen.js";
 import gameLauncher from "./modules/game-launcher.js";
+import gameReview from "./modules/gameReview.js";
 import screensSizer from "./modules/screens-sizer";
 
 screensSizer();
 gameLauncher();
 new DemoBar();
+gameReview().init();
 // disableScreen({ title: "test", message: "test"})
 
 window.disableScreen = disableScreen;
