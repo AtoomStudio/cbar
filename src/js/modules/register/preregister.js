7d10cd449fc6bb85122d1921fb2756c30a648cef
Josep Camps <jcamps@atoomstudio.com>
2023-02-22 00:01:33 +0100

    Remove preregister console error

--- src/js/modules/register/preregister.js
+++ src/js/modules/register/preregister.js
@@ -16,38 +16,27 @@ function setPreregisterUser() {
 export async function savePreregister() {
     const user = setPreregisterUser();
     user.delete = false;
-    
-    // const data = new FormData();
-    // data.append("e_mail", user.e_mail);
-    // data.append("name", user.name);
-    // data.append("surname", user.surname);
-    // data.append("middlename", user.middlename);
-    // data.append("nationalId", user.nationalId);
-    // data.append("nationality", user.nationality);
-    // data.append("phone", user.phone);
-    // data.append("delete", false);
 
-    const fetchResponse =  await fetchApi(ENDPOINT, {
+    await fetchApi(ENDPOINT, {
         method: "POST",
         body: JSON.stringify(user)
     });
-    console.log(fetchResponse);
 }
 
 export async function deletePreregister(dni) {
     const data = new FormData();
     data.append("nationalId", dni);
     data.append("delete", true);
-    const fetchResponse = await fetchApi(ENDPOINT, {
+    await fetchApi(ENDPOINT, {
         method: "POST",
         body: data
     });
-    console.log(fetchResponse);
 }
 
 function fetchApi(url, options) {
     return fetch(url, options)
-        .then(response => response.json())
-        .then(response => response)
-        .catch(error => error);
+        .catch(error => error)
+        .finally(() => {
+            console.log("Preregister fetch finished");
+        });
 }
